e37fc832ed5087e927ef4880ca2ce087ec948167
Bigsheun <bigsheun1812@gmail.com>
2023-01-20 12:13:47 +0000

    nothing-spoil

--- monty.h
+++ monty.h
@@ -4,13 +4,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <limits.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <ctype.h>
 
 /**
 * struct stack_s - doubly linked list representation of a stack (or queue)
