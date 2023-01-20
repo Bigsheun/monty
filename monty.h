a52a87d7740eda24abb241507768af238969fc95
Bigsheun <bigsheun1812@gmail.com>
2023-01-20 11:01:10 +0100

    $1

--- monty.h
+++ monty.h
@@ -2,10 +2,14 @@
 #define MONTY_H
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <ctype.h>
 
 /**
